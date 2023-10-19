faeb4be3210347d255b2bb29fbc5fecf6f1c2b00
Hieueee <hieubui457@gmail.com>
2023-10-18 23:06:22 -0700

    add interrupt routine to control relay sequences

--- code/relayControls/relayControls.ino
+++ code/relayControls/relayControls.ino
@@ -1,39 +1,58 @@
 #include <Arduino.h>
 #include <wiring.h>
 
-#define RELAY_1 2
+#define RELAY_1 8
 #define RELAY_2 3
 #define RELAY_3 4
 #define RELAY_4 5
 #define RELAY_5 6
 #define RELAY_6 7
+#define fireTrigger 2
+#define purgeTrigger 1
+
 relayPins[] = {RELAY_1,RELAY_2,RELAY_3,RELAY_4,RELAY_5,RELAY_6};
 
-// relay states
-static boolean state1 = 0;
-static boolean state2 = 0;
-static boolean state3 = 0;
-static boolean state4 = 0;
-static boolean state5 = 0;
-static boolean state6 = 0;
+unsigned long previousTime;  //some global variables available anywhere in the program
+unsigned long currentMillis;
 
-unsigned int fireTime = 10000;
+// interrupt pins
+volatile bool fireState;
+volatile bool purgeState;
+
+// relay states
+static bool state1 = 0;
+static bool state2 = 0;
+static bool state3 = 0;
+static bool state4 = 0;
+static bool state5 = 0;
+static bool state6 = 0;
+
+const unsigned int fireTime = 10000;
+const unsigned int purgeTime = 6000;
 unsigned int fireStart = 0;
-String command;
+
+int command;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-delay(1000);
+  Serial.begin(115200);
+  delay(1000);
 
-pinMode(RELAY_1, OUTPUT);
-pinMode(RELAY_2, OUTPUT);
-pinMode(RELAY_3, OUTPUT);
-pinMode(RELAY_4, OUTPUT);
-pinMode(RELAY_5, OUTPUT);
-pinMode(RELAY_6, OUTPUT);
+  pinMode(RELAY_1, OUTPUT);
+  pinMode(RELAY_2, OUTPUT);
+  pinMode(RELAY_3, OUTPUT);
+  pinMode(RELAY_4, OUTPUT);
+  pinMode(RELAY_5, OUTPUT);
+  pinMode(RELAY_6, OUTPUT);
 
+  pinMode(fireTrigger,INPUT_PULLUP);
+  pinMode(purgeTrigger,INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(fireTrigger),startSeq,FALLING);
+  attachInterrupt(digitalPinToInterrupt(purgeTrigger),purge,FALLING);
 }
+
+
 
 void relaysCal()
 {
@@ -47,60 +66,76 @@ void relaysCal()
 
 void startSeq()
 {
-    digitalWrite(RELAY_5, HIGH)
-    digitalWrite(RELAY_6, HIGH)
-    delay(10000); // 10 seconds fire
-    digitalWrite(RELAY_5, LOW)
-    digitalWrite(RELAY_6, LOW) 
+  if(digitalRead(fireTrigger)==LOW)
+  {
+    fireState =! fireState;
+    digitalWrite(RELAY_5, fireState);
+    digitalWrite(RELAY_6, fireState);
+  }
 }
 
 void purge()
 {
-    digitalWrite(RELAY_3,HIGH);
-    digitalWrite(RELAY_4,HIGH);
-    digitalWrite(RELAY_5, HIGH)
-    digitalWrite(RELAY_6, HIGH)
-    delay(6000);
-    digitalWrite(RELAY_3,HIGH);
-    digitalWrite(RELAY_4,HIGH);
+  if(digitalRead(purgeTrigger)==LOW)
+  {
+    purgeState =! purgeState;
+    digitalWrite(RELAY_3,purgeState);
+    digitalWrite(RELAY_4,purgeState);
+    digitalWrite(RELAY_5, purgeState);
+    digitalWrite(RELAY_6, purgeState);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  currentMillis = currentMillis();
   Serial.println("Enter commands: ");
-  while(Serial.available()>0)
+  if (Serial.available()>0){
+    command = Serial.parseInt();                 
+  }
+  switch (command) {
+    case 1: // relay 1 on
+      state1 = !state1;
+      digitalWrite(RELAY_1, state1);
+    case 2: // relay 2 on
+      state2 = !state2;
+      digitalWrite(RELAY_2, state2);
+    case 3: // relay 3 on
+      state3 = !state3;
+      digitalWrite(RELAY_3, state3);
+    case 4: // relay 4 on
+      state4 = !state4;
+      digitalWrite(RELAY_4, state4);
+    case 5: // relay 5 on
+      state5 = !state5;
+      digitalWrite(RELAY_5, state5);
+    case 6: // relay 6 on
+      state6 = !state6;
+      digitalWrite(RELAY_6, state6);
+    case 7:
+      relaysCal();
+    case 8:
+      digitaWrite(fireTrigger, digitalRead(fireTrigger) ^ 1);
+    case 9:
+      digitalWrite(purgeTrigger, digitalRead(purgeTrigger) ^ 1);
+    default:
+      break;
+  }
+  if(digitalRead(fireTrigger) == 0)
   {
-    command = Serial.readString();  
-    command.trim();                        
-    switch (command) {
-      case "r1": // relay 1 on
-        state1 = !state1;
-        digitalWrite(RELAY_1, state1);
-      case "r2": // relay 2 on
-        state2 = !state2;
-        digitalWrite(RELAY_2, state2);
-      case "r3": // relay 3 on
-        state3 = !state3;
-        digitalWrite(RELAY_3, state3);
-      case "r4": // relay 4 on
-        state4 = !state4;
-        digitalWrite(RELAY_4, state4);
-      case "r5": // relay 5 on
-        state5 = !state5;
-        digitalWrite(RELAY_5, state5);
-      case "r6": // relay 6 on
-        state6 = !state6;
-        digitalWrite(RELAY_6, state6);
-      case "purge":
-        purge();
-      case "fire":
-        startSeq();
-      case "cal":
-        relaysCal();
-      default:
-        break;
+    if(currentMillis - previousTime >= fireTime)
+    {
+      digitaWrite(fireTrigger, digitalRead(purgeTrigger) ^ 1);
+      previousTime = currentMillis;
     }
-      
+  }
+  if(digitalRead(purgeTrigger) == 0)
+  {
+    if(currentMillis - previousTime >= purgeTime)
+    {
+      digitaWrite(purgeTrigger, digitalRead(purgeTrigger) ^ 1);
+      previousTime = currentMillis;
+    }
   }
 }
 
